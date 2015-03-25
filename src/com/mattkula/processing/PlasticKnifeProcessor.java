7c52db0d16a574869ef13731a90d184efafcedd1
Matt Kula <matthewrkula@gmail.com>
2015-03-25 15:28:37 -0500

    Bind annotation can bind views in activities

--- src/com/mattkula/processing/PlasticKnifeProcessor.java
+++ src/com/mattkula/processing/PlasticKnifeProcessor.java
@@ -18,10 +18,11 @@ import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import javax.tools.JavaFileObject;
 
+import com.mattkula.processing.annotations.Bind;
 import com.mattkula.processing.annotations.SetValue;
 
 @SupportedAnnotationTypes(value= {"*"})
-@SupportedSourceVersion(SourceVersion.RELEASE_6)
+@SupportedSourceVersion(SourceVersion.RELEASE_7)
 public class PlasticKnifeProcessor extends AbstractProcessor {
 	
 	public static final String INJECTOR_SUFFIX = "Injector";
@@ -39,6 +40,17 @@ public class PlasticKnifeProcessor extends AbstractProcessor {
 	@Override
 	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
 		
+//		processSetValue(roundEnv);
+		processBind(roundEnv);
+
+		return false;
+	}
+	
+	public void addImport(String packageName) {
+		plasticKnifeHeader += "import " + packageName + ";\n";
+	}
+	
+	private void processSetValue(RoundEnvironment roundEnv) {
 		if (!roundEnv.processingOver()) {
 			Set<? extends Element> elements = roundEnv.getElementsAnnotatedWith(SetValue.class);
 			
@@ -68,7 +80,7 @@ public class PlasticKnifeProcessor extends AbstractProcessor {
 
 				JavaFileObject file = null;
 				try {
-					file = filer.createSourceFile("com/mattkula/processing/" + className);
+					file = filer.createSourceFile("com.mattkula.processing." + className);
 					file.openWriter()
 					.append(plasticKnifeHeader)
 					.append(plasticKnifeContent)
@@ -79,12 +91,49 @@ public class PlasticKnifeProcessor extends AbstractProcessor {
 
 			}
 		}
-
-		return false;
-	}
-	
-	public void addImport(String packageName) {
-		plasticKnifeHeader += "import " + packageName + ";\n";
 	}
 
+	private void processBind(RoundEnvironment roundEnv) {
+		if (!roundEnv.processingOver()) {
+			Set<? extends Element> elements = roundEnv.getElementsAnnotatedWith(Bind.class);
+			
+			for (Element element : elements) {
+				Element parent = element.getEnclosingElement();
+				String parentName = parent.getSimpleName().toString();
+
+				if (!parentToChildrenMap.keySet().contains(parentName)) {
+					parentToChildrenMap.put(parentName, new ArrayList<Element>());
+					addImport(parent.asType().toString());
+				}
+				parentToChildrenMap.get(parentName).add(element);
+			}
+
+			for (String classElement : parentToChildrenMap.keySet()) {
+				String className = classElement + INJECTOR_SUFFIX;
+				String plasticKnifeContent = String.format("public class %s implements Injector<%s> {\n\n", className, classElement);
+
+				plasticKnifeContent += String.format("\tpublic void inject(%s injector) {\n", classElement);
+				List<Element> childElements = parentToChildrenMap.get(classElement);
+
+				for (Element element : childElements) {
+					Bind annotation = element.getAnnotation(Bind.class);
+					plasticKnifeContent += String.format("\t\tinjector.%s = (%s)injector.findViewById(%d);\n", 
+							element.getSimpleName(), element.asType(), annotation.value());
+				}
+				plasticKnifeContent += "\t}\n\n" + "}\n";
+
+				JavaFileObject file = null;
+				try {
+					file = filer.createSourceFile("com.mattkula.processing." + className);
+					file.openWriter()
+					.append(plasticKnifeHeader)
+					.append(plasticKnifeContent)
+					.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+			}
+		}
+	}
 }
